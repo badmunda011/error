5439f9e612372c1c883b42cc64aa55051e3bdc53
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-07 18:16:34 +0530

    Update info.py

--- MukeshRobot/modules/info.py
+++ MukeshRobot/modules/info.py
@@ -4,7 +4,7 @@ from MukeshRobot import pbot
 from MukeshRobot.utils.errors import capture_err
 from MukeshRobot.modules.helper_funcs.misc import is_module_loaded
 
-__mod_name__ = "ɪɴғᴏʀᴍᴀᴛɪᴏɴs"
+__mod_name__ = "ɪɴғᴏʀᴍᴀᴛɪᴏɴ"
 
 __help__ = """
 **Iɴғᴏʀᴍᴀᴛɪᴏɴ**
