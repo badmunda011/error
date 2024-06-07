d9fc334cf12bc87e65688660baa71fedc84ff4d0
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-07 23:23:00 +0530

    Create ranking.py

---
+++ MukeshRobot/modules/ranking.py
@@ -0,0 +1,14 @@
+from pyrogram import filters
+
+from MukeshRobot import pbot
+from MukeshRobot.utils.errors import capture_err
+from MukeshRobot.modules.helper_funcs.misc import is_module_loaded
+
+__mod_name__ = "ʀᴀɴᴋɪɴɢ"
+
+__help__ = """
+
+Gʀᴏᴜᴘ ᴍᴇᴍʙᴇʀ ᴄᴏᴜɴᴛ ᴍsɢ .
+ ❍ /ranking ᴛᴏᴅᴀʏ ᴍsɢ ʟɪsᴛ , ᴏᴠᴇʀᴀʟʟ ᴍsɢ ʟɪsᴛ .
+ """
+ 
