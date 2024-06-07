01805bc5323fc5a31a7da0a3b060363c5392260a
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-07 23:22:04 +0530

    Create couples.py

---
+++ MukeshRobot/modules/couples.py
@@ -0,0 +1,15 @@
+from pyrogram import filters
+
+from MukeshRobot import pbot
+from MukeshRobot.utils.errors import capture_err
+from MukeshRobot.modules.helper_funcs.misc import is_module_loaded
+
+__mod_name__ = "ᴄᴏᴜᴘʟᴇs"
+
+__help__ = """
+
+Cʜᴏᴏsᴇ ᴄᴏᴜᴘʟᴇs ɪɴ ʏᴏᴜʀ ᴄʜᴀᴛ
+ ❍ /couple, /couples, /shipping : Cʜᴏᴏsᴇ 2 ᴜsᴇʀs ᴀɴᴅ sᴇɴᴅ ᴛʜᴇɪʀ ɴᴀᴍᴇs ᴀs ᴄᴏᴜᴘʟᴇs ɪɴ ʏᴏᴜʀ ᴄʜᴀᴛ.
+
+ """
+
