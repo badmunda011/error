1d72fe088105ecc784670aeb4696b50299a06d5a
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-07 23:20:13 +0530

    Create chatnot.py

---
+++ MukeshRobot/modules/chatnot.py
@@ -0,0 +1,14 @@
+from pyrogram import filters
+
+from MukeshRobot import pbot
+from MukeshRobot.utils.errors import capture_err
+from MukeshRobot.modules.helper_funcs.misc import is_module_loaded
+
+__mod_name__ = "ᴄʜᴀᴛʙᴏᴛ"
+
+__help__ = """
+
+❍ /chatbot ➛ ᴀɪ ᴄʜᴀᴛʙᴏᴛ ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ
+
+ """
+
