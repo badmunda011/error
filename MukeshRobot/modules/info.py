3bff7afeec814b46e5acbf1bb7de6dd5b3ae0a39
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-07 16:29:08 +0530

    Create info.py

---
+++ MukeshRobot/modules/info.py
@@ -0,0 +1,14 @@
+from pyrogram import filters
+
+from MukeshRobot import pbot
+from MukeshRobot.utils.errors import capture_err
+from MukeshRobot.modules.helper_funcs.misc import is_module_loaded
+
+__mod_name__ = "ɪɴғᴏʀᴍᴀᴛɪᴏɴ"
+
+__help__ = """
+**Iɴғᴏʀᴍᴀᴛɪᴏɴ**
+
+• /ɪɴғᴏ - Tᴏ ɢᴇᴛ ɪɴғᴏ ᴀʙᴏᴜᴛ ᴛʜᴇ ᴜsᴇʀ
+• /ᴄʜɪɴғᴏ - Tᴏ ɢᴇᴛ ɪɴғᴏ ᴀʙᴏᴜᴛ ᴛʜᴇ ᴄʜᴀᴛ
+"""
