9973249221f9b178270f21f6e49407606ee8bbfe
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-07 16:31:11 +0530

    Create formatting.py

---
+++ MukeshRobot/modules/formatting.py
@@ -0,0 +1,11 @@
+from pyrogram import filters
+
+from MukeshRobot import pbot
+from MukeshRobot.utils.errors import capture_err
+from MukeshRobot.modules.helper_funcs.misc import is_module_loaded
+
+__mod_name__ = "ғᴏʀᴍᴀᴛᴛɪɴɢ"
+
+__help__ = """
+**ғᴏʀᴍᴀᴛᴛɪɴɢ**
+sᴜᴘᴘᴏʀᴛs ᴀ ʟᴀʀɢᴇ ɴᴜᴍʙᴇʀ ᴏғ ғᴏʀᴍᴀᴛᴛɪɴɢ ᴏᴘᴛɪᴏɴs ᴛᴏ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴍᴇssᴀɢᴇs ᴍᴏʀᴇ ᴇxᴘʀᴇssɪᴠᴇ. Tᴀᴋᴇ ᴀ ʟᴏᴏᴋ ʙʏ ᴄʟɪᴄᴋɪɴɢ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ʙᴇʟᴏᴡ! """
