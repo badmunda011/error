7e9b47193be026fe67cfc2ae8400a08566134658
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-07 16:36:19 +0530

    Create font.py

---
+++ MukeshRobot/modules/font.py
@@ -0,0 +1,12 @@
+from pyrogram import filters
+
+from MukeshRobot import pbot
+from MukeshRobot.utils.errors import capture_err
+from MukeshRobot.modules.helper_funcs.misc import is_module_loaded
+
+__mod_name__ = "Fᴏɴᴛ"
+
+__help__ = """
+ ❍ /fonts <text> *:* ᴄᴏɴᴠᴇʀᴛs sɪᴍᴩʟᴇ ᴛᴇxᴛ ᴛᴏ ʙᴇᴀᴜᴛɪғᴜʟ ᴛᴇxᴛ ʙʏ ᴄʜᴀɴɢɪɴɢ ɪᴛ's ғᴏɴᴛ.
+ """
+ 
