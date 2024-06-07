4c32ae07a3a3c8f560d5b2d71fdc2e7132d291d3
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-07 14:11:59 +0530

    Update users.py

--- MukeshRobot/modules/users.py
+++ MukeshRobot/modules/users.py
@@ -1,24 +1,10 @@
-from io import BytesIO
-from time import sleep
 from pyrogram import filters
-from pyrogram.types import Message
-from telegram import TelegramError, Update
-from telegram.error import BadRequest, Unauthorized
+
+from MukeshRobot import pbot
+from MukeshRobot.utils.errors import capture_err
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler
-import MukeshRobot.modules.no_sql.users_db as user_db 
-from MukeshRobot import pbot as Mukesh
-from MukeshRobot import DEV_USERS, LOGGER as  logger, OWNER_ID, dispatcher
 from MukeshRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from MukeshRobot.modules.no_sql.users_db import get_all_users
-from pyrogram import Client
-from pyrogram.types import Message
-from pyrogram.errors import (
-    FloodWait,
-    InputUserDeactivated,
-    UserIsBlocked,
-    PeerIdInvalid,
-)
-import time, asyncio, logging, datetime
+
 
 
 
