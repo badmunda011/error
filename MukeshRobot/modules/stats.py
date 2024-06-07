34bc6e5eb8e33a5784da86ae800299d74b3700db
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-07 16:21:55 +0530

    Create stats.py

---
+++ MukeshRobot/modules/stats.py
@@ -0,0 +1,14 @@
+from pyrogram import filters
+
+from MukeshRobot import pbot
+from MukeshRobot.utils.errors import capture_err
+from MukeshRobot.modules.helper_funcs.misc import is_module_loaded
+
+__mod_name__ = "sᴛᴀᴛs"
+
+__help__ = """
+❅ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs ꜰᴏʀ sᴛᴀᴛs ❅
+
+ ❍ /sᴛᴀᴛs. ʙᴏᴛ sᴛᴀᴛs (ʙᴏᴛ ᴅᴇᴀᴛɪʟ)
+ """
+ 
