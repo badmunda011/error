8d82452ae5baed2e88f275d3f9b1a896b8b26422
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-07 23:24:01 +0530

    Create bug.py

---
+++ MukeshRobot/modules/bug.py
@@ -0,0 +1,15 @@
+from pyrogram import filters
+
+from MukeshRobot import pbot
+from MukeshRobot.utils.errors import capture_err
+from MukeshRobot.modules.helper_funcs.misc import is_module_loaded
+
+__mod_name__ = "ʙᴜɢ"
+
+__help__ = """
+
+✿ ғᴏʀ ʀᴇᴩᴏʀᴛɪɴɢ ᴀ  ʙᴜɢ ✿
+
+ ❍ /bug ➛ ᴛᴏ ʀᴇᴩᴏʀᴛ ᴀ ʙᴜɢ ᴀᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ.
+ """
+ 
