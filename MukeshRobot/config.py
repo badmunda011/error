1747fd33adb9d39477a09d9a47948972bead69e4
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-08 13:13:32 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -7,14 +7,14 @@ class Config(object):
     API_ID = "" # integer value, dont use ""
     API_HASH = ""
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 6762113050 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 6898413162 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "MASTIWITHFRIENDSXD"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "PBX_PERMOT"  # Your own group for support, do not add the @
     START_IMG = ""
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= ""
     # RECOMMENDED
-BOT_USERNAME = getenv("BOT_USERNAME" , "PoliceBot")
+BOT_USERNAME = getenv("BOT_USERNAME" , "BrokenRobot_Bot")
     DATABASE_URL = getenv("DATABASE_URL", " postgres://iarfggbc:Vxzh_kG7cxa1kHR5faxcd1kuA4R-UT9E@rosie.db.elephantsql.com/iarfggbc")
     CASH_API_KEY = (
         ""  # Get your API key from https://www.alphavantage.co/support/#api-key
