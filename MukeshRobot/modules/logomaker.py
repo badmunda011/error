f57015821281e4071f68241483d4c0e8a9c49db1
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-08 13:04:01 +0530

    Update logomaker.py

--- MukeshRobot/modules/logomaker.py
+++ MukeshRobot/modules/logomaker.py
@@ -1746,7 +1746,7 @@ async def lego(event):
     if os.path.exists(fname):
         os.remove(fname)
     except Exception as e:
-        await event.reply(f"ᴇʀʀᴏʀ {e}, ʀᴇᴩᴏʀᴛ ᴛʜɪs ᴀᴛ @{SUPPORT_CHAT} ")
+    await event.reply(f"ᴇʀʀᴏʀ {e}, ʀᴇᴩᴏʀᴛ ᴛʜɪs ᴀᴛ @{SUPPORT_CHAT} ")
         
 
 @register(pattern="^/ylogo ?(.*)")
